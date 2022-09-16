20458b193fdf6c3aa17b374e353df62c30cbb7bf
Terry Amagboro <rukkiecodes@gmail.com>
2022-09-17 00:55:54 +0100

    ADMIN SEES ALL USERS LENGTH

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,5 +1,12 @@
-(() => {
-  let token = localStorage.userToken
-  // if (!token || token == undefined) location.replace('login.html')
-  // else location.replace('/')
-})()
+const token = localStorage.userToken;
+const total_number_of_users = document.querySelector('.total_number_of_users');
+
+// count all registrd usrs
+(async () => {
+  const users = await axios({
+    method: 'get',
+    url: 'https://trustpaddi-waitlist.herokuapp.com/admin/allUsers',
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+  total_number_of_users.innerText = users.data.users.length
+ })()
